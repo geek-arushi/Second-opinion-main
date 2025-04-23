4c336f874c5924943c8cdc58660bdfe5b3310313
geek-arushi <arushisingh1121@gmail.com>
2025-04-23 16:45:57 +0530

    SecOpp

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -35,7 +35,9 @@ const Header = () => {
                         <div className="row align-items-center">
                             <div className="col-6">
                                 <div className="header-logo">
-                                    <Logo image={`${process.env.PUBLIC_URL}/img/logo.png`} />
+                                    <Logo
+                                        image={`${process.env.PUBLIC_URL}/img/logo.png`}
+                                    />
                                 </div>
                             </div>
                             <div className="col-6 d-flex justify-content-end">
@@ -52,13 +54,17 @@ const Header = () => {
                                         aria-expanded={offcanvasShow}
                                         type="button"
                                     >
-                                        <svg viewBox="0 0 800 600" aria-hidden="true">
-                                            <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top" />
-                                            <path d="M300,320 L540,320" id="middle" />
+                                        <svg
+                                            viewBox="0 0 800 600"
+                                            aria-hidden="true"
+                                        >
                                             <path
-                                                d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190"
-                                                id="bottom"
-                                                transform="translate(480, 320) scale(1, -1) translate(-480, -318)"
+                                                d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200"
+                                                id="top"
+                                            />
+                                            <path
+                                                d="M300,320 L540,320"
+                                                id="middle"
                                             />
                                         </svg>
                                     </button>
@@ -97,7 +103,9 @@ const Header = () => {
                             <div className="col-lg-12">
                                 <div className="d-flex flex-wrap align-items-top justify-content-between">
                                     <div className="header-logo2">
-                                        <Logo image={`${process.env.PUBLIC_URL}/img/logo.png`} />
+                                        <Logo
+                                            image={`${process.env.PUBLIC_URL}/img/logo.png`}
+                                        />
                                     </div>
                                     <MainMenu classOption="small-menu" />
                                     <Button
